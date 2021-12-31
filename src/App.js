e1e26316f4b8bd29c26e57748321c918797d0f89
Aditya <Adi72Hzb@gmail.com>
2021-12-31 23:18:11 +0530

    FrontEndDummyDone

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router , Route, Routes} from 'react-router-dom';
+import React ,{ useState , useCallback } from 'react';
+import { BrowserRouter as Router , Route, Routes , Navigate } from 'react-router-dom';
 
 import NewPlace from './places/pages/NewPlace';
 import Users from './users/pages/Users';
@@ -7,22 +7,54 @@ import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './users/pages/Auth';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
+  const [isLoggedIn , setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if(isLoggedIn){
+    routes =(
+      <>
+        <Route path="/" element={<Users/>} exact/>
+        <Route path="/:userId/places" element ={<UserPlaces/>} exact/>
+        <Route path="/places/new" element={<NewPlace/>} exact/>
+        <Route path="/places/:placeId" element={<UpdatePlace/>}></Route>
+        <Route path="*" element={<Navigate replace to="/"/>}/>
+      </>
+    );
+  }else{
+    routes = (
+      <>
+        <Route path="/" element={<Users/>} exact/>
+        <Route path="/:userId/places" element ={<UserPlaces/>} exact/>
+        <Route path="/auth" element={<Auth/>}></Route>
+        <Route path="*" element={<Navigate replace to="/"/>}/>
+      </>
+    );
+  }
+
   return (
-  <Router>
-      <MainNavigation/>
-    <main>
-      <Routes>
-            <Route path="/" element={<Users/>} exact/>
-            <Route path="/:userId/places" element ={<UserPlaces/>} exact/>
-            <Route path="/places/new" element={<NewPlace/>} exact/>
-            <Route path="/places/:placeId" element={<UpdatePlace/>}></Route>
-            <Route path="/auth" element={<Auth/>}></Route>
-      </Routes>
-    </main>
-     
-  </Router>
+    <AuthContext.Provider value={{isLoggedIn : isLoggedIn , login : login , logout:logout}}>
+        <Router>
+        <MainNavigation/>
+      <main>
+        <Routes>
+          {routes}
+        </Routes>
+      </main>
+    </Router>
+    </AuthContext.Provider>
+  
   );
 };
 
